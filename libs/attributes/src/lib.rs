7f4b501f5e01e754bee2d249805b0b5c85540019
Evan Gipson <evangipson@gmail.com>
2025-05-17 12:17:57 -0500

    Add explainer for http_get attribute

--- libs/attributes/src/lib.rs
+++ libs/attributes/src/lib.rs
@@ -5,10 +5,35 @@ use syn::{ItemFn, ReturnType, Type, parse_macro_input};
 fn extract_return_type(a: ReturnType) -> Box<Type> {
     match a {
         ReturnType::Type(_, p) => p,
-        _ => panic!("Not supported on functions without return types!"),
+        _ => panic!("http_get is not supported on functions without return types!"),
     }
 }
 
+#[doc = r#"
+    # http_get
+    The [`#[http_get]`](http_get) attribute modifies the function that uses it inline
+    to return a [`Route`](http::route), as long as the function returns a [`String`].
+
+    # example
+    [`#[http_get]`](http_get) can be used to hook up a [`String`] as a [`Route`](http::route).
+    
+    The following function:
+    ```rust
+    use attributes::http_get;
+
+    #[http_get("/")]
+    fn some_request() -> String {
+        "hello".to_string()
+    }
+    ```
+
+    Will be parsed and returned as:
+    ```rust
+    fn some_request() -> http::route::Route {
+        http::route::Route::get("/", "hello".to_string())
+    }
+    ```
+"#]
 #[proc_macro_attribute]
 pub fn http_get(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(item as ItemFn);
